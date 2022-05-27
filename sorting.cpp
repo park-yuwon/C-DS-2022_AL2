a03168b4779c13a5b7d70058e700395829819651
park-yuwon <77564623+park-yuwon@users.noreply.github.com>
2022-05-27 15:51:07 +0900

    Update sorting.cpp

--- sorting.cpp
+++ sorting.cpp
@@ -26,12 +26,15 @@
 
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> // rand()함수 포함 라이브러리
+#include <time.h> // time()함수 포함 라이브러리
 #include <string.h>
 #include <stdbool.h>
 #include <memory.h>
 
 #define MAX_STR 10000
+
+#define MAX_RAND_NUM 30000
 
 struct RANDS {
 	FILE* pos ;
@@ -94,6 +97,159 @@ bool fileWriter2(char* fileName, const int arrType, const void* arr, const int l
 	return true;
 }
 
+bool file_w(char* filename, int size, int* arr)
+{
+	FILE* infile = fopen(filename, "w");
+	if (infile == NULL)
+	{
+		printf("\n File cannot be opened\n");
+		return false;
+		exit(1);
+	}
+	else
+	{
+		for (int i = 0; i < size; i++)
+		{
+			fprintf(infile, " %d ", arr[i]);
+		}
+	}
+	fclose(infile);
+	return true;
+}
+
+bool file_w_double(char* filename, int size, double* arr)
+{
+	FILE* infile = fopen(filename, "w");
+	if (infile == NULL)
+	{
+		printf("\n File cannot be opened\n");
+		return false;
+		exit(1);
+	}
+	else
+	{
+		for (int i = 0; i < size; i++)
+		{
+			fprintf(infile, " %lf ", arr[i]);
+		}
+	}
+}
+
+bool file_r(char* filename, int* arr)
+{
+	FILE* infile = fopen(filename, "r");
+	if (infile == NULL)
+	{
+		printf("\n File cannot be opened\n ");
+		exit(1);
+		return false;
+
+	}
+	else
+	{
+		int num;
+		int i = 0;
+		while (fscanf(infile, "%d", &num) == 1)
+		{
+			arr[i] = num;
+			i++;
+		}
+
+	}
+	fclose(infile);
+	return true;
+}
+
+bool file_r_double(char* filename, double* arr)
+{
+	FILE* infile = fopen(filename, "r");
+	if (infile == NULL)
+	{
+		printf("\n File cannot be opened\n ");
+		exit(1);
+		return false;
+
+	}
+	else
+	{
+		double num;
+		int i = 0;
+		while (fscanf(infile, "%lf", &num) == 1)
+		{
+			arr[i] = num;
+			i++;
+		}
+
+	}
+	fclose(infile);
+	return true;
+}
+
+bool file_string_w(char* filename, int size, char str[][6])
+{
+	FILE* infile;
+	infile = fopen(filename, "w");
+	if (infile == NULL)
+	{
+		printf("file cannot be opened\n");
+		exit(0);
+		return false;
+	}
+
+	else
+	{
+		for (int i = 0; i < size; i++)
+		{
+			fprintf(infile, " %s ", str[i]);
+		}
+		fclose(infile);
+	}
+	return true;
+}
+
+bool file_string_r(char* filename, char str[][6])
+{
+	FILE* infile;
+	infile = fopen(filename, "r");
+	if (infile == NULL)
+	{
+		printf("file cannot be oepened\n");
+		exit(0);
+		return false;
+	}
+
+	else
+	{
+		char randomstr[6];
+		int i = 0;
+		while (fscanf(infile, "%s", &str) == 1)
+		{
+			strcpy(str[i], randomstr);
+			i++;
+		}
+		return true;
+	}
+}
+
+void printarr(int* arr, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		printf(" %d ", arr[i]);
+	}
+}
+
+void printarr_double(double* arr, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		printf(" %lf ", arr[i]);
+	}
+}
+
+
+
+
 
 /*
  Part1: Implement the following random input generator
@@ -124,11 +280,17 @@ bool fx_rand(const int n) {
  Category2: Merge sort, Quick sort
  Category3: Heap sort, Radix sort
 */
+
 void insertion_s() {
-	int arr[] = { 12, 11, 13, 5, 6 }, int n = sizeof(arr) / sizeof(arr[0]);
+	int arr[] = { 12, 11, 13, 5, 6 };
+	// 12 11 13 5 6
+	// 11 12 13 5 6
+	// 11 12 13 5 6
+	// 5  11 12 13 6
+	// 5  6  11 12 13
+	int n = sizeof(arr) / sizeof(arr[0]);
 	int i, key, j;
-	for (i = 1; i < n; i++)
-	{
+	for (i = 1; i < n; i++) {
 		key = arr[i];
 		j = i - 1;
 
@@ -172,18 +334,71 @@ void selection_s() {
 	}
 
 }
+
+
+void swap(int* xp, int* yp)
+{
+	int temp = *xp;
+	*xp = *yp;
+	*yp = temp;
+}
+
+void swap_double(double* xp, double* yp)
+{
+	double temp = *xp;
+	*xp = *yp;
+	*yp = temp;
+}
+
+void selectionSort(int arr[], int n)
+{
+	int i, j, min_idx;
+
+	// One by one move boundary of unsorted subarray
+	for (i = 0; i < n - 1; i++)
+	{
+		// Find the minimum element in unsorted array
+		min_idx = i;
+		for (j = i + 1; j < n; j++)
+			if (arr[j] < arr[min_idx])
+				min_idx = j;
+
+		// Swap the found minimum element with the first element
+		swap(&arr[min_idx], &arr[i]);
+	}
+}
+
+void selectionSort_double(double arr[], int n)
+{
+	int i, j, min_idx;
+
+	// One by one move boundary of unsorted subarray
+	for (i = 0; i < n - 1; i++)
+	{
+		// Find the minimum element in unsorted array
+		min_idx = i;
+		for (j = i + 1; j < n; j++)
+			if (arr[j] < arr[min_idx])
+				min_idx = j;
+
+		// Swap the found minimum element with the first element
+		swap_double(&arr[min_idx], &arr[i]);
+	}
+}
+/*
 void bubble_s() {
 
 }
 void merge_s() {
 
-}
+}*/
 
 /* This function takes last element as pivot, places
 the pivot element at its correct position in sorted
 array, and places all smaller (smaller than pivot)
 to left of pivot and all greater elements to right
 of pivot */
+/*
 int partition(int arr[], int low, int high)
 {
 	int pivot = arr[high]; // pivot
@@ -207,8 +422,8 @@ void quick_s() {
 
 	if (low < high)
 	{
-		/* pi is partitioning index, arr[p] is now
-		at right place */
+		//pi is partitioning index, arr[p] is now
+		//at right place 
 		int pi = partition(arr, low, high);
 
 		// Separately sort elements before
@@ -223,7 +438,7 @@ void heap_s(FILE* fd) {
 void radix_s(FILE* fd) {
 
 }
-
+*/
 /*
 *
  Part3: Implement the following sorting evaluator:
@@ -232,6 +447,7 @@ void radix_s(FILE* fd) {
  Level3: The running time
  Level4: The stability correctness
 */
+/*
 bool result_load_and_saver(FILE* in) {
 
 }
@@ -240,16 +456,20 @@ bool sort_corrector(FILE* fd) {
 }
 bool running_timer(FILE* fd) {
 
+	// add logic to measure the time clock difference
+	clock_gettime(CLOCK_MONOTONIC, &finish);
+	elapsed = (finish.tv_sec - start.tv_sec); // seconds
+	elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0; // nanoseconds
 }
 bool stability_corrector(FILE* fd) {
 
 }
-
+*/
 int main() {
+
+	// Part 1. Create Random numbers file
+	/*
 	RANDS *rands;
-
-
-
 
 	if (!(rands->pos = pos_rand(10000))) {
 		printf("Cannot Create Positive Random numbers File\n");
@@ -267,16 +487,134 @@ int main() {
 		printf("Cannot Create Fixed Length Random numbers File\n");
 		exit(1);
 	}
+	*/
 
 
-	insertion_s();
+	int arr[MAX_RAND_NUM];
+	int minusarr[1000];
+	double doublearr[1000];
+
+	srand(time(NULL)); // 매번 다른 시드값 생성
+	int random = 0; // 정수형 변수 선언
+	double randomf = 0.0; //실수형 변수 선언
+
+	for (int i = 0; i < MAX_RAND_NUM; i++) { // 10번 반복
+		random = rand(); // 난수 생성
+		//arr[i] = random / 100;
+		arr[i] = random;
+	}
+
+	random = 0;
+	for (int i = 0; i < 1000; i++)
+	{
+		random = rand();
+		minusarr[i] = -random / 100;
+
+	}
+
+	randomf = 0;
+	for (int i = 0; i < 1000; i++) {
+		randomf = rand();
+
+		doublearr[i] = ((double)rand() * (2000)) / (double)RAND_MAX - 1000;
+	}
+
+	char code_str[1000][6];
+
+	int type;
+
+	for (int k = 0; k < 1000; k++)
+	{
+		for (int i = 0; i < 5; i++)
+		{
+			type = rand() % 2;
+			if (type == 0)
+			{
+				code_str[k][i] = 'a' + rand() % 26;
+			}
+			else
+				code_str[k][i] = 'A' + rand() % 26;
+
+		}
+		code_str[k][5] = '\0';
+	}
+
+
+
+	// File Write
+
+	int size = sizeof(arr) / sizeof(arr[0]);
+
+	char fname[20] = "testint.txt";
+	char fname2[20] = "testminus.txt";
+	char fname3[20] = "tesetdouble.txt";
+	char fname4[20] = "testtext.txt";
+
+	file_w(fname, size, arr);
+	/*file_w(fname2, size, minusarr);
+	file_w_double(fname3, size, doublearr);
+	file_string_w(fname4, size, code_str);*/
+
+	// File Read
+
+	int newarr[MAX_RAND_NUM];
+	int newminusarr[1000];
+	double newdoublearr[1000];
+	char newcodestr[1000][6];
+
+
+	file_r(fname, newarr);
+	//file_r(fname2, newminusarr);
+	//file_r_double(fname3, newdoublearr);
+	//file_string_r(fname4, newcodestr);
+
+	/*
+	printf("1-1. Print newarr \n");
+	printarr(newarr, size);
+	printf("\n1-2. Print newminusarr \n");
+	printarr(newminusarr, size); // 0 ~ 음수
+	printf("\n1-3. Print newdoublearr \n");
+	printarr_double(newdoublearr, size);
+	*/	
+	
+	// Part 2. Sort algorithms
+
+	/*insertion_s();
 	selection_s();
 	bubble_s();
 	merge_s();
 	quick_s();
 	heap_s();
-	radix_s();
+	radix_s();*/
 
+
+	double start, end;
+	start = (double)clock() / CLOCKS_PER_SEC;
+
+	selectionSort(newarr, size);
+
+	end = (((double)clock()) / CLOCKS_PER_SEC);
+	printf("프로그램 수행 시간 :%lf\n", (end - start));
+
+
+	selectionSort(newarr, size);
+	//selectionSort(newminusarr, size);
+	//selectionSort_double(newdoublearr, size);
+	//selectionSort(newcodestr,size); 문자열(level4)요거 정렬 함수 짜야댐
+
+	
+	/*
+	printf("2-1. Print sorted newarr \n");
+	printarr(newarr, size);
+	printf("\n2-2. Print sorted newminusarr \n");
+	printarr(newminusarr, size); // 0 ~ 음수
+	printf("\n2-3. Print sorted newdoublearr \n");
+	printarr_double(newdoublearr, size);
+	*/
+
+
+	// 
+	// 
 	//close(fd);
 
 	printf("Hello");
